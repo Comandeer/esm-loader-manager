72d9c8e3673c0461ee531b9c24b865d65fdb073f
Comandeer <vepomoc@gmail.com>
2022-03-29 23:53:31 +0200

    feat(utilities): add `resolveProjectRoot()` helper

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,6 @@
+import { resolve as resolvePath } from 'path';
 import { fileURLToPath } from 'url';
+import { readdir } from 'fs/promises';
 import { readFile } from 'fs/promises';
 
 function createModuleURL( specifier, { parentURL } ) {
@@ -15,5 +17,24 @@ async function loadURL( url ) {
 	return readFile( path );
 }
 
+async function resolveProjectRoot( startDir ) {
+	const files = await readdir( startDir );
+
+	if ( files.includes( 'package.json' ) ) {
+		return startDir;
+	}
+
+	const dirUp = resolvePath( startDir, '..' );
+
+	// If directory one level up is the same as the current on,
+	// we're at / and there's nowhere to go up.
+	if ( dirUp === startDir ) {
+		return null;
+	}
+
+	return resolveProjectRoot( dirUp );
+}
+
 export { createModuleURL };
 export { loadURL };
+export { resolveProjectRoot };
--- tests/utilities.js
+++ tests/utilities.js
@@ -5,12 +5,16 @@ import { resolve as resolvePath } from 'path';
 import test from 'ava';
 import { createModuleURL } from '../src/utilities.js';
 import { loadURL } from '../src/utilities.js';
+import { resolveProjectRoot } from '../src/utilities.js';
 
 const __dirname = dirname( fileURLToPath( import.meta.url ) );
 const fixtureDirPath = resolvePath( __dirname, '__fixtures__' );
 const dummyPath = resolvePath( fixtureDirPath, 'dummy.txt' );
+const simpleLoaderDirPath = resolvePath( fixtureDirPath, 'simpleLoader' );
+const nestedLoaderDirPath = resolvePath( fixtureDirPath, 'nested' );
+const nestedLoaderLevel3DirPath = resolvePath( nestedLoaderDirPath, 'level1', 'level2', 'level3' );
 
-test( '#createModuleURL() creates URL from the given specifier', ( t ) => {
+test( 'createModuleURL() creates URL from the given specifier', ( t ) => {
 	const specifier = './test.js';
 	const expectedURL = new URL( specifier, import.meta.url ).href;
 	const url = createModuleURL( specifier, {
@@ -20,10 +24,22 @@ test( '#createModuleURL() creates URL from the given specifier', ( t ) => {
 	t.deepEqual( url, expectedURL );
 } );
 
-test( '#loadURL() loads given file as a buffer', async ( t ) => {
+test( 'loadURL() loads given file as a buffer', async ( t ) => {
 	const fileURL = pathToFileURL( dummyPath );
 	const expectedFileContent = Buffer.from( 'hublabubla\n' );
 	const fileContent = await loadURL( fileURL );
 
 	t.deepEqual( fileContent, expectedFileContent );
+} );
+
+test( 'resolveProjectRoot() points to the directory with the nearest package.json file (same)', async ( t ) => {
+	const resolvedProjectRoot = await resolveProjectRoot( simpleLoaderDirPath );
+
+	t.is( resolvedProjectRoot, simpleLoaderDirPath );
+} );
+
+test( 'resolveProjectRoot() points to the directory with the nearest package.json file (nested)', async ( t ) => {
+	const resolvedProjectRoot = await resolveProjectRoot( nestedLoaderLevel3DirPath );
+
+	t.is( resolvedProjectRoot, nestedLoaderDirPath );
 } );
