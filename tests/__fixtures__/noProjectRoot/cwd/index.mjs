4c8a227c3900697eaff265de2366736205e01d89
Comandeer <vepomoc@gmail.com>
2022-05-08 11:43:27 +0200

    test(fixtures): rename incorrectly named assertion

--- tests/__fixtures__/noProjectRoot/cwd/index.mjs
+++ tests/__fixtures__/noProjectRoot/cwd/index.mjs
@@ -1,7 +1,7 @@
 import assertion from './module.mjs';
 import nestedAssertion from './subdir/module.mjs';
-import outputAssertion from '../outsideCWD/module.mjs';
+import outsideAssertion from '../outsideCWD/module.mjs';
 
 console.log( assertion );
 console.log( nestedAssertion );
-console.log( outputAssertion );
+console.log( outsideAssertion );
