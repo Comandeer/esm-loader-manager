1db27b6742b4b311697427ada36df1197fb44116
Comandeer <vepomoc@gmail.com>
2022-05-12 00:52:15 +0200

    feat(esmlm): add support for running in CWD and passing entrypoint as parameter

--- bin/esmlm.js
+++ bin/esmlm.js
@@ -1,1 +1,24 @@
 #!/usr/bin/env node
+
+import { argv } from 'process';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { resolve as resolvePath } from 'path';
+import { execa } from 'execa';
+
+const __dirname = dirname( fileURLToPath( import.meta.url ) );
+const loaderPath = resolvePath( __dirname, '..', 'dist', 'esm-loader-manager.mjs' );
+const cwd = process.cwd();
+const [ ,, passedEntryPoint ] = argv;
+const entryPoint = passedEntryPoint ? resolvePath( cwd, passedEntryPoint ) : cwd;
+const cmd = 'node';
+const params = [
+	'--experimental-loader',
+	loaderPath,
+	entryPoint
+];
+
+execa( cmd, params, {
+	cwd,
+	stdio: 'inherit'
+} );
