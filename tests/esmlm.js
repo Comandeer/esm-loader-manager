17521f9a1a27a1037f2e7e260e2cde17280960ef
Comandeer <vepomoc@gmail.com>
2022-05-12 00:51:43 +0200

    test(esmlm): add additional tests

--- tests/esmlm.js
+++ tests/esmlm.js
@@ -7,10 +7,51 @@ import createEsmlmTest from './__helpers__/createEsmlmTest.js';
 const __dirname = dirname( fileURLToPath( import.meta.url ) );
 const fixtureDirPath = resolvePath( __dirname, '__fixtures__' );
 const esmlmFixturePath = resolvePath( fixtureDirPath, 'esmlm' );
+const entryPointPath = resolvePath( esmlmFixturePath, 'index.js' );
+const successfulExitCode = 0;
 
-test( 'esmlm correctly launches Node.js module without any parameters and with set CWD', createEsmlmTest( {
+test( 'esmlm correctly launches Node.js module without any parameters', createEsmlmTest( {
 	cwd: esmlmFixturePath,
-	callback( t, { stdout } ) {
+	callback( t, { stdout, exitCode } ) {
 		t.is( stdout, 'true' );
+		t.is( exitCode, successfulExitCode );
+	}
+} ) );
+
+test( 'esmlm correctly launches Node.js module with the absolute path as the parameter', createEsmlmTest( {
+	cwd: esmlmFixturePath,
+	entryPoint: entryPointPath,
+	callback( t, { stdout, exitCode } ) {
+		t.is( stdout, 'true' );
+		t.is( exitCode, successfulExitCode );
+	}
+} ) );
+
+test( 'esmlm correctly launches Node.js module with the relative path as the parameter', createEsmlmTest( {
+	cwd: esmlmFixturePath,
+	entryPoint: 'index.js',
+	callback( t, { stdout, exitCode } ) {
+		t.is( stdout, 'true' );
+		t.is( exitCode, successfulExitCode );
+	}
+} ) );
+
+test( 'esmlm correctly launches Node.js module with the "." as the parameter', createEsmlmTest( {
+	cwd: esmlmFixturePath,
+	entryPoint: '.',
+	callback( t, { stdout, exitCode } ) {
+		t.is( stdout, 'true' );
+		t.is( exitCode, successfulExitCode );
+	}
+} ) );
+
+test( 'esmlm throws error when incorrect relative path is passed as the parameter', createEsmlmTest( {
+	cwd: esmlmFixturePath,
+	entryPoint: './non-existent.js',
+	callback( t, { stderr, exitCode } ) {
+		const moduleNotFoundError = /[ERR_MODULE_NOT_FOUND]/;
+
+		t.regex( stderr, moduleNotFoundError );
+		t.not( exitCode, 0 );
 	}
 } ) );
