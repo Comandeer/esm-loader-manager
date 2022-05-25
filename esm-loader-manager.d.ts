f2f790cbd72bedcc7bf3fc57b142f64ad74e5c16
Comandeer <vepomoc@gmail.com>
2022-05-26 00:07:07 +0200

    chore(typings): provide sensible typings

--- esm-loader-manager.d.ts
+++ esm-loader-manager.d.ts
@@ -1,1 +1,16 @@
-export {};
+import { Buffer } from 'node:buffer';
+
+export interface MatcherContext {
+	conditions: Array<string>;
+	importAssertions: Record<string, unknown>;
+	parentURL: string | undefined;
+}
+
+export interface Loader {
+	matcher( url: string, context: MatcherContext ): boolean;
+	loader( url: string, source: Buffer | string ): Buffer | string;
+}
+
+export interface LoaderConfiguration {
+	loaders: Array<Loader>;
+}
