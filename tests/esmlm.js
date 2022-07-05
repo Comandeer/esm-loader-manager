f55df6b4dca87314ead0a84964bfa8eaa378c96f
Comandeer <vepomoc@gmail.com>
2022-07-05 22:21:19 +0200

    feat(esmlm): pass additional arguments to the underlying program

--- tests/esmlm.js
+++ tests/esmlm.js
@@ -7,8 +7,14 @@ import createEsmlmTest from './__helpers__/createEsmlmTest.js';
 const __dirname = dirname( fileURLToPath( import.meta.url ) );
 const fixtureDirPath = resolvePath( __dirname, '__fixtures__' );
 const esmlmFixturePath = resolvePath( fixtureDirPath, 'esmlm' );
-const entryPointPath = resolvePath( esmlmFixturePath, 'index.js' );
+const esmlmFixtureEntryPointPath = resolvePath( esmlmFixturePath, 'index.js' );
+const esmlmArgsFixturePath = resolvePath( fixtureDirPath, 'esmlmArgs' );
+const esmlmArgsFixtureEntryPointPath = resolvePath( esmlmArgsFixturePath, 'index.js' );
 const successfulExitCode = 0;
+const sampleArgs = [
+	'some-path',
+	'--some-arg'
+];
 
 test( 'esmlm correctly launches Node.js module without any parameters', createEsmlmTest( {
 	cwd: esmlmFixturePath,
@@ -20,7 +26,7 @@ test( 'esmlm correctly launches Node.js module without any parameters', createEs
 
 test( 'esmlm correctly launches Node.js module with the absolute path as the parameter', createEsmlmTest( {
 	cwd: esmlmFixturePath,
-	entryPoint: entryPointPath,
+	entryPoint: esmlmFixtureEntryPointPath,
 	callback( t, { stdout, exitCode } ) {
 		t.is( stdout, 'true' );
 		t.is( exitCode, successfulExitCode );
@@ -53,5 +59,16 @@ test( 'esmlm throws error when incorrect relative path is passed as the paramete
 
 		t.regex( stderr, moduleNotFoundError );
 		t.not( exitCode, 0 );
+	}
+} ) );
+
+// #2
+test.only( 'esmlm correctly passes arguments to the underlying program', createEsmlmTest( {
+	cwd: esmlmArgsFixturePath,
+	entryPoint: esmlmArgsFixtureEntryPointPath,
+	args: sampleArgs,
+	callback( t, { stdout, exitCode } ) {
+		t.is( stdout, sampleArgs.join( ' ' ) );
+		t.is( exitCode, successfulExitCode );
 	}
 } ) );
