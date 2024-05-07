da34db542610424aa322cdcd4f4c2465e0f74d8a
Comandeer <comandeer@comandeer.pl>
2024-05-08 00:52:15 +0200

    chore(dist): clean after release [ci skip]

--- dist/register.mjs
+++
@@ -1,3 +0,0 @@
-/*! esm-loader-manager v0.6.0 | (c) 2024 Comandeer | MIT license (see LICENSE) */
-import{register as m}from"node:module";m("./index.mjs",import.meta.url);
-//# sourceMappingURL=register.mjs.map
